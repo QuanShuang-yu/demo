d0683ef6b52272684481dfa9460e8bb69b9af895
QuanShuang-yu <563834409@qq.com>
2019-12-28 16:51:14 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+		System.out.println("3");
+		System.out.println("Hello World");
+		System.out.println("4");
+		System.out.println("Hello World");
+		System.out.println("Hello Java");
+		System.out.println("1");
+		System.out.println("Hello Java");
+		System.out.println("2");
+		System.out.println("Hello Java");
+    }
+}
